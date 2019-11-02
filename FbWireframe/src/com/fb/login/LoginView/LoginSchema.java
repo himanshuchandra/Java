57ee10cab58cc1693e5470131fbffea03d8eff71
Prashant Jain <prashantjain.pro@gmail.com>
2019-11-02 08:42:12 +0530

    changes for login schema

---
+++ FbWireframe/src/com/fb/login/LoginView/LoginSchema.java
@@ -0,0 +1,18 @@
+package com.fb.login.LoginView;
+
+import java.util.Scanner;
+
+import com.fb.login.LoginDTO.LoginDTO;
+import com.fb.login.LoginDTO.ObjectsDto;
+import com.fb.login.LoginHelper.LoginHelper;
+
+public class LoginView {
+	
+		
+	public static void main(String[] args){
+		LoginView view = new LoginView();
+		view.doLogin();
+	}
+	
+}
+
